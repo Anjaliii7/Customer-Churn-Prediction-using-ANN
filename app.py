3dd4095f16b615fae83c267a0ffd28e5c2707800
Anjali Jhanjhariya <168601185+Anjaliii7@users.noreply.github.com>
2025-08-21 05:16:12 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,79 @@
+import streamlit as st 
+import numpy as np
+import tensorflow as tf
+from sklearn.preprocessing import StandardScaler,LabelEncoder,OneHotEncoder
+import pandas as pd 
+import pickle
+
+model=tf.keras.models.load_model('model.keras')       #loading trained model
+
+#load the trained model ,scaler pickle, onehot encoder
+
+with open('onehot_encoder_geo.pkl','rb')as file:
+    onehot_encoder_geo=pickle.load(file)
+
+with open('label_encoder_gender.pkl','rb')as file:
+    label_encoder_gender=pickle.load(file)
+
+with open('scaler.pkl','rb')as file:
+    scaler=pickle.load(file)
+
+#streamlit app
+st.title('Customer Churn Prediction')
+ 
+#user input
+geography=st.selectbox('Geography',onehot_encoder_geo.categories_[0])
+gender=st.selectbox('Gender',label_encoder_gender.classes_)
+age=st.slider('Age',18,92)
+balance=st.number_input('Balance')
+credit_score=st.number_input('Credit Score')
+estimated_salary=st.number_input('Estimated Salary')
+tenure=st.slider('Tenure',0,10)
+num_of_products=st.slider('Number of Products',1,4)
+has_cr_card=st.selectbox('Has Credit Card',[0,1])
+is_active_member=st.selectbox('Is Active Member',[0,1])
+
+#prepare the input data
+input_data=pd.DataFrame({
+    'CreditScore':[credit_score],
+    'Gender':[label_encoder_gender.transform([gender])[0]],
+    'Age':[age],
+    'Tenure':[tenure],
+    'Balance':[balance],
+    'NumOfProducts':[num_of_products],
+    'HasCrCard':[has_cr_card],
+    'IsActiveMember':[is_active_member],
+    'EstimatedSalary':[estimated_salary]
+})
+
+#one hot encode geo
+geo_encoded=onehot_encoder_geo.fit_transform([[geography]]).toarray()
+geo_encoded_df=pd.DataFrame(geo_encoded,columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
+
+# Get all expected columns from training time
+expected_geo_columns = ['Geography_France', 'Geography_Germany', 'Geography_Spain']
+
+# Make sure all columns exist in geo_encoded_df, fill missing with 0
+for col in expected_geo_columns:
+    if col not in geo_encoded_df.columns:
+        geo_encoded_df[col] = 0
+
+# Reorder columns to match training order
+geo_encoded_df = geo_encoded_df[expected_geo_columns]
+
+# combine one hot encoded columns with inout data
+input_data=pd.concat([input_data.reset_index(drop=True),geo_encoded_df],axis=1)
+
+#scale the input data
+input_data_scaled=scaler.transform(input_data)
+
+#prediction churn
+prediction=model.predict(input_data_scaled)
+prediction_proba=prediction[0][0]
+
+st.write(f'Churn Probalility: {prediction_proba: .2f}')
+
+if prediction_proba>0.5:
+    st.write('The customer is likely to churn')
+else:
+    st.write('The customer is not likely to churn')
